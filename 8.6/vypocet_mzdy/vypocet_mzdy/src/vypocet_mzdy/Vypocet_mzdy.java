1faa6a0d441d9dc91789d1009edec6aa42506738
Milan.zlamal.qa@gmail.com <milan.zlamal.qa@gmail.com>
2017-07-27 18:41:21 +0200

    Add files via upload

---
+++ 8.6/vypocet_mzdy/vypocet_mzdy/src/vypocet_mzdy/Vypocet_mzdy.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vypocet_mzdy;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Milan Z
+ */
+public class Vypocet_mzdy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*vstup pres klavku
+        vypocet ciste mzdy progresivnim zpusobem
+        pres switch
+        report printem
+        */
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Zadej vysi tve hrube mzdy");
+        float mujPlat = keyboard.nextFloat();
+        
+        float progresivniDan = 0;
+               
+        float dostanete = 0;
+        //druhy napad
+        
+        if (mujPlat < 0 && !(mujPlat <= 1000)) {
+            Scanner keyboard1 = new Scanner(System.in);
+            System.out.println("Zadej vysi tve hrube mzdy");
+            mujPlat = keyboard.nextFloat();
+        }
+        
+        if (mujPlat <= 10000) {
+            dostanete = mujPlat - 1000; 
+            progresivniDan = mujPlat;
+        }
+        
+        if (mujPlat <= 20000 && mujPlat > 10000) {
+           progresivniDan = (float) (1000 + (mujPlat * 0.2));
+           dostanete = mujPlat - progresivniDan;
+        }
+        
+        if (mujPlat <= 30000 && mujPlat > 20000) {
+            progresivniDan = (float) (3000 + (mujPlat * 0.3));
+            dostanete = mujPlat - progresivniDan;
+        }
+        if (mujPlat <= 40000 && mujPlat > 30000) {
+            progresivniDan = (float) (6000 + (mujPlat * 0.4));
+            dostanete = mujPlat - progresivniDan;
+        }
+        dostanete = mujPlat - progresivniDan;
+        System.out.println("Progresivni dan je: " + progresivniDan + "\n" + "dostanete :" + dostanete);
+    }
+    
+}
